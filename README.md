6e4ed77c0420ca2638bf59460d2a2ecf9be1781a
Alex Nepomuceno <sosaalex707@gmail.com>
2024-01-29 15:24:44 -0500

    Update

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
-# caeser.py
-Creating a code that will be able to encrypt a message to a code 
+# Ceaser's Cipher
+- In this project, our aim is to design and implement a robust encryption and decryption system. Our program will offer the functionality to encode text using a secure encryption algorithm, rendering the original content unreadable to anyone without the proper decryption key. The encryption process will involve converting the input text into a format that is indecipherable without the corresponding decryption method.
+
+To ensure the security of the encoded data, we will employ industry-standard encryption techniques, making it virtually impossible for unauthorized parties to access the original content. Additionally, our system will provide users with a seamless decryption experience, allowing them to easily retrieve the original text from the encoded form using the decryption key.
+
+One of the key features of our program is its user-friendly interface, which will enable users to interact with the encryption and decryption functions intuitively. We will prioritize usability, aiming to make the process of encrypting and decrypting text as straightforward as possible for our users.
+
+Furthermore, our program will be designed with flexibility in mind, allowing users to customize certain aspects of the encryption process to suit their specific needs. This could include options such as choosing the encryption algorithm or providing a custom decryption key for added security.
+
+In summary, our project will deliver a comprehensive encryption and decryption solution that prioritizes security, usability, and flexibility. Users can expect a reliable and efficient tool for securing their sensitive information with confidence."
+
+
+![Alt text](ceaser.gif)![Alt text](Julius-Caesar-marble-sculpture-Andrea-di-Pietro.webp)
--- caesar.py
+++ caesar.py
@@ -1,33 +1,72 @@
 # We are going to create cypher text using ASCII values
+# We are also going to create a text to be decrypted
 user_input = input("Enter a message to encrypt: ")
+user_decrypt = input("Enter a message to decrypt: ")
 
-# we are going to create a class called Cypher so we can use it to encrypt
+
+#We are going to create a class called Cypher so we can use it to encrypt
 class Cypher:
 	
-	# This function will take the user input and convert it to ASCII values
+#This function will take the user input and convert it to ASCII values
 	def convert_to_ascii(user_input):
-		ascii_list = [] # We are going to use this to store the values in the ASCII 
-		for i in user_input:# We are going to use a for loop to go throught each letter the user input 
-			ascii_list.append(ord(i))# we are going to use this to add the value to the list and what the number is going to be 
-		return ascii_list# This is going to return the acsii list when we call it 
+		ascii_list = [] 
+		for i in user_input:
+			ascii_list.append(ord(i))
+		return ascii_list
 
-	# This function will take the ASCII values and convert them to characters
+#This function will take the ASCII values and convert them to characters
 	def convert_to_char(ascii_list):
-		char_list = []# we are going to make a list to save our progress inside 
-		for i in ascii_list:# we are going to create a list for every letter in the code 
-			char_list.append(chr(i))# this code we are going to add(.append) the chracter to the list that we created to stay organized 
-		return char_list# this will return the list we created and saved the values in
+		char_list = ""
+		for i in ascii_list:
+			char_list += (chr(i))
+		return char_list
 
-	# This function will take the ASCII values and shift them by 10 places to the right
-	def shift(ascii_list): # we are to use this function to be able to shift the values by 10 places
-		shift_list = []# Here we are gonna save it in a list so we can use it later
-		for i in ascii_list: # we are going to use a loop here to go through each character in the code 
-			shift_list.append(i + 10) # as soon as we get a letter then we are going to add a 10 to it 
-		return shift_list # this will return the list we created and saved the values in
+#This function will take the ASCII values and shift them by 10 places to the right
+	def shift(ascii_list):
+		shift_list = []
+		for i in ascii_list:
+			if i == 32:
+				shift_list.append(i)
+				continue # The reason for this is to make sure the code does not shift the space
+			shifted = i + 10
+			if shifted > 122:  #This will loop around for lowercase letters
+				shifted -= 26
+			elif shifted > 90 and i < 91:  #This will loop around for uppercase letters
+				shifted -= 26
+			shift_list.append(shifted)
+		return shift_list 
+	
+# This Function will take the ASCII values and shift them by 10 places to the left(back to normal) Just copy and paste from
+# The code above and just remove the shift + 10 
+	def unshift(ascii_list):
+		unshift_list = []
+		for i in ascii_list:
+			if i == 32:
+				unshift_list.append(i)
+				continue
+			unshifted = i - 10
+			if unshifted < 97 and i > 96:  #This will loop around for lowercase letters
+				unshifted += 26
+			elif unshifted < 65:  #This will loop around for uppercase letters
+				unshifted += 26
+			unshift_list.append(unshifted)
+		return unshift_list
+	
 
 # In this code we are going to call the function we created and additionally we are going to print the encrypted message
-# The cypher text comes from the class we created so in order to use the class we are going to call it everytime we want to use what is inside the class
-# the reason why we are going to add a . after the class is because we are going to use the function inside the class
+# The Cypher text comes from the class we created so in order to use the class we are going to call it everytime we want to use what is inside the class
+# reason why we are going to add a . after the class is because we are going to use the function inside the class
 # the reason why also is added everythign together instead of having it seperate it to make the code more cleaner and more organized
-print(Cypher.convert_to_char(Cypher.shift(Cypher.convert_to_ascii(user_input)))) 
+# We are also going to ask the user to input a message to decrypt
+	
+#In this code we are going to call the function we created. This will help use stay organize and clean 
+# We call that chaining functions.(We are calling the class(Cypher)within a function by using a .)
+	
+encrypted_message = Cypher.convert_to_char(Cypher.shift(Cypher.convert_to_ascii(user_input)))
+print(f"Encrypted message: {encrypted_message}")
 
+# We are going to call the function we created to decrypt the message
+#we are also chaining so we do not make a mess of the code.
+
+decryption =Cypher.convert_to_char(Cypher.unshift(Cypher.convert_to_ascii(user_decrypt)))
+print(f"Decrypted message: {decryption}")
